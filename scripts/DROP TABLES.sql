690681f57f15bf82fafe02cb9084cb648905eed3
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-08-29 19:40:46 -0400

    Drop table script

---
+++ scripts/DROP TABLES.sql
@@ -0,0 +1,39 @@
+DROP TABLE categoriamantencion CASCADE CONSTRAINTS;
+DROP TABLE categoriareporte CASCADE CONSTRAINTS;
+DROP TABLE categoriaservicio CASCADE CONSTRAINTS;
+DROP TABLE cliente CASCADE CONSTRAINTS;
+DROP TABLE conector_departamento_servicio CASCADE CONSTRAINTS;
+DROP TABLE conector_departamento_servicioextra CASCADE CONSTRAINTS;
+DROP TABLE conector_servicioextra_reserva CASCADE CONSTRAINTS;
+DROP TABLE departamento CASCADE CONSTRAINTS;
+DROP TABLE error CASCADE CONSTRAINTS;
+DROP TABLE estadopago CASCADE CONSTRAINTS;
+DROP TABLE estadoreserva CASCADE CONSTRAINTS;
+DROP TABLE estadousuario CASCADE CONSTRAINTS;
+DROP TABLE foto CASCADE CONSTRAINTS;
+DROP TABLE iventario CASCADE CONSTRAINTS;
+DROP TABLE mantencion CASCADE CONSTRAINTS;
+DROP TABLE pago CASCADE CONSTRAINTS;
+DROP TABLE permiso CASCADE CONSTRAINTS;
+DROP TABLE reporte CASCADE CONSTRAINTS;
+DROP TABLE reserva CASCADE CONSTRAINTS;
+DROP TABLE servicio CASCADE CONSTRAINTS;
+DROP TABLE servicioextra CASCADE CONSTRAINTS;
+DROP TABLE sesion CASCADE CONSTRAINTS;
+DROP TABLE usuario CASCADE CONSTRAINTS;
+
+DROP SEQUENCE cliente_id_usuario_seq;
+DROP SEQUENCE departamento_id_departamento;
+DROP SEQUENCE error_id_error_seq;
+DROP SEQUENCE foto_id_foto_seq;
+DROP SEQUENCE iventario_id_inventario_seq;
+DROP SEQUENCE mantencion_id_mantencion_seq;
+DROP SEQUENCE pago_id_pago_seq;
+DROP SEQUENCE reporte_id_reporte_seq;
+DROP SEQUENCE reserva_id_reserva_seq;
+DROP SEQUENCE servicio_id_servicio_seq;
+DROP SEQUENCE servicioextra_id_servicioextra;
+DROP SEQUENCE sesion_id_sesion_seq;
+DROP SEQUENCE usuario_id_usuario_seq;
+
+
