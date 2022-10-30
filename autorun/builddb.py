812637e4e29b72e49987ce406ca799042b81ead3
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-10-29 23:27:21 -0300

    read nested commands

--- autorun/builddb.py
+++ autorun/builddb.py
@@ -15,25 +15,43 @@ def readScript(path):
     with open(path) as f:
         raw = f.readlines()
         lastLine = ""
+        nestedLvl = 0
         for line in raw:
-            line = line.replace("\n", "")
+            line = line.replace("\n", "").replace("/", "")
             if ("--" in line or line == ""):
                 continue
             if (";" in line):
+                if "end" in line.lower():
+                    nestedLvl -= 1
+                if nestedLvl == 0:
+                    if "end" not in line.lower():
+                        line = line.replace(";","")
                     script.append(lastLine + line)
                     lastLine = ""
-            else:
-                lastLine = lastLine + line + " "
+                    continue
+            
+            lastLine = lastLine + line + " "
+            if "begin" in line.lower():
+                nestedLvl += 1
+                    
             
     return script
 
 def runCommand(con, command):
-    command = command.replace(";","")
+    print(command)
     with con.cursor() as cursor:
         cursor.execute(command)
 
+def createUser():
+    with oracledb.connect(sysCS) as connection:
+        runScrpit(connection, "../scripts/USR_TURISMO_REAL.sql")
 
-with oracledb.connect(sysCS) as connection:
-    runScrpit(connection, "../scripts/USR_TURISMO_REAL.sql")
+def fillDB():
+    with oracledb.connect(turCS) as connection:
+        runScrpit(connection, "../scripts/CREATE TABLES.sql")
+        runScrpit(connection, "../scripts/BASIC DATA.sql")
+        runScrpit(connection, "../scripts/REGIONES_Y_COMUNAS.sql")
 
-
+if __name__ == "__main__":
+    createUser()
+    fillDB()
